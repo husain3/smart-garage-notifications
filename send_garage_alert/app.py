1184dac986cc789a02cdd9b22979a7d397049b6b
Arshad Husain <arshad.husain1@gmail.com>
2021-04-04 02:48:10 -0600

    Add SES support to send texts via email
    This helps save money by sending the more frequent text messages through
    a free SMS service by telus rather than directly via AWS SNS.

--- send_garage_alert/app.py
+++ send_garage_alert/app.py
@@ -3,27 +3,10 @@ import boto3
 import os
 
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
+def lambda_handler(event, context):    
+    ses_client = boto3.client("ses", region_name="us-west-2")
+    CHARSET = "UTF-8"
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
     sns_client = boto3.client('sns')
 
     if(event['queryStringParameters'] is None):
@@ -31,7 +14,6 @@ def lambda_handler(event, context):
             "statusCode": 400,
             "body": json.dumps({
                 "message": json.dumps({"status": "Bad Request Error", "message": "Malformed client request. No params specified. Please provide one door_status, and one open_duration (if applicable) in params"}),
-                # "location": ip.text.replace("\n", "")
             }),
         }
 
@@ -39,16 +21,46 @@ def lambda_handler(event, context):
         sns_client = boto3.client('sns')
 
         if('door_status' in event['queryStringParameters'].keys() and event['queryStringParameters']['door_status'] == 'opened'):
-            response = sns_client.publish(
-                TopicArn=os.environ["SNSArn"],
-                Message='Garage door is now open',
-                Subject='Door Activity'
+            response = ses_client.send_email(
+                Destination={
+                    "ToAddresses": [
+                        os.environ["SESEmail"],
+                    ],
+                },
+                Message={
+                    "Body": {
+                        "Text": {
+                            "Charset": CHARSET,
+                            "Data": "Garage door has been opened",
+                        }
+                    },
+                    "Subject": {
+                        "Charset": CHARSET,
+                        "Data": "GARAGE DOOR OPENED",
+                    },
+                },
+                Source=os.environ["SESEmail"],
             )
         elif('door_status' in event['queryStringParameters'].keys() and event['queryStringParameters']['door_status'] == 'closed'):
-            response = sns_client.publish(
-                TopicArn=os.environ["SNSArn"],
-                Message='Garage door is now closed',
-                Subject='Door Activity'
+            response = ses_client.send_email(
+                Destination={
+                    "ToAddresses": [
+                        os.environ["SESEmail"],
+                    ],
+                },
+                Message={
+                    "Body": {
+                        "Text": {
+                            "Charset": CHARSET,
+                            "Data": "Garage door has been closed",
+                        }
+                    },
+                    "Subject": {
+                        "Charset": CHARSET,
+                        "Data": "GARAGE DOOR CLOSED",
+                    },
+                },
+                Source=os.environ["SESEmail"],
             )
         elif(event['queryStringParameters']['door_status'] == 'still_open' and ('open_duration' in event['queryStringParameters'].keys() and event['queryStringParameters']['open_duration'].isnumeric())):
             response = sns_client.publish(
@@ -61,16 +73,13 @@ def lambda_handler(event, context):
                 "statusCode": 400,
                 "body": json.dumps({
                     "message": json.dumps({"status": "Bad Request Error", "message": "Malformed client request. Please provide one door_status, and one open_duration (if applicable) in params"}),
-                    # "location": ip.text.replace("\n", "")
                 }),
             }
 
         return {
             "statusCode": 200,
             "body": json.dumps({
-                # "message": "SNS message published successfully",
-                "message": "SNS message published successfully",
-                # "location": ip.text.replace("\n", "")
+                "message": "Message sent successfully",
             }),
         }
     except Exception as e:
