85b4bdf1c66a37e1f0f8718372139184119547cd
Arshad Husain <arshad.husain1@gmail.com>
2021-04-05 00:40:25 -0600

    Capitalize garage door statuses

--- send_garage_alert/app.py
+++ send_garage_alert/app.py
@@ -4,88 +4,89 @@ import os
 
 
 def lambda_handler(event, context):    
-    ses_client = boto3.client("ses", region_name="us-west-2")
-    CHARSET = "UTF-8"
+	ses_client = boto3.client("ses", region_name="us-west-2")
+	CHARSET = "UTF-8"
 
-    sns_client = boto3.client('sns')
+	sns_client = boto3.client('sns')
 
-    if(event['queryStringParameters'] is None):
-        return {
-            "statusCode": 400,
-            "body": json.dumps({
-                "message": json.dumps({"status": "Bad Request Error", "message": "Malformed client request. No params specified. Please provide one door_status, and one open_duration (if applicable) in params"}),
-            }),
-        }
+	if(event['queryStringParameters'] is None):
+		return {
+			"statusCode": 400,
+			"body": json.dumps({
+				"message": json.dumps({"status": "Bad Request Error", "message": "Malformed client request. No params specified. Please provide one door_status, and one open_duration (if applicable) in params"}),
+			}),
+		}
 
-    try:
-        sns_client = boto3.client('sns')
+	try:
+		sns_client = boto3.client('sns')
 
-        if('door_status' in event['queryStringParameters'].keys() and event['queryStringParameters']['door_status'] == 'opened'):
-            response = ses_client.send_email(
-                Destination={
-                    "ToAddresses": [
-                        os.environ["SESEmail"],
-                    ],
-                },
-                Message={
-                    "Body": {
-                        "Text": {
-                            "Charset": CHARSET,
-                            "Data": "Garage door has been opened",
-                        }
-                    },
-                    "Subject": {
-                        "Charset": CHARSET,
-                        "Data": "GARAGE DOOR OPENED",
-                    },
-                },
-                Source=os.environ["SESEmail"],
-            )
-        elif('door_status' in event['queryStringParameters'].keys() and event['queryStringParameters']['door_status'] == 'closed'):
-            response = ses_client.send_email(
-                Destination={
-                    "ToAddresses": [
-                        os.environ["SESEmail"],
-                    ],
-                },
-                Message={
-                    "Body": {
-                        "Text": {
-                            "Charset": CHARSET,
-                            "Data": "Garage door has been closed",
-                        }
-                    },
-                    "Subject": {
-                        "Charset": CHARSET,
-                        "Data": "GARAGE DOOR CLOSED",
-                    },
-                },
-                Source=os.environ["SESEmail"],
-            )
-        elif(event['queryStringParameters']['door_status'] == 'still_open' and ('open_duration' in event['queryStringParameters'].keys() and event['queryStringParameters']['open_duration'].isnumeric())):
-            response = sns_client.publish(
-                TopicArn=os.environ["SNSArn"],
-                Message=f"Garage door has been open for {event['queryStringParameters']['open_duration']} minutes",
-                Subject='Door Activity'
-            )
-        else:
-            return {
-                "statusCode": 400,
-                "body": json.dumps({
-                    "message": json.dumps({"status": "Bad Request Error", "message": "Malformed client request. Please provide one door_status, and one open_duration (if applicable) in params"}),
-                }),
-            }
+		if('door_status' in event['queryStringParameters'].keys() and event['queryStringParameters']['door_status'] == 'OPENED'):
+			response = ses_client.send_email(
+				Destination={
+					"ToAddresses": [
+						os.environ["SESEmail"],
+					],
+				},
+				Message={
+					"Body": {
+						"Text": {
+							"Charset": CHARSET,
+							"Data": "Garage door has been opened",
+						}
+					},
+					"Subject": {
+						"Charset": CHARSET,
+						"Data": "GARAGE DOOR OPENED",
+					},
+				},
+				Source=os.environ["SESEmail"],
+			)
 
-        return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "message": "Message sent successfully",
-            }),
-        }
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": json.dumps({
-                "message": json.dumps({"status": "Lambda Error", "message": f"Lambda error message - {str(e)}"}),
-            }),
-        }
+		elif('door_status' in event['queryStringParameters'].keys() and event['queryStringParameters']['door_status'] == 'CLOSED'):
+			response = ses_client.send_email(
+				Destination={
+					"ToAddresses": [
+						os.environ["SESEmail"],
+					],
+				},
+				Message={
+					"Body": {
+						"Text": {
+							"Charset": CHARSET,
+							"Data": "Garage door has been closed",
+						}
+					},
+					"Subject": {
+						"Charset": CHARSET,
+						"Data": "GARAGE DOOR CLOSED",
+					},
+				},
+				Source=os.environ["SESEmail"],
+			)
+		elif(event['queryStringParameters']['door_status'] == 'STILL_OPEN' and ('open_duration' in event['queryStringParameters'].keys() and event['queryStringParameters']['open_duration'].isnumeric())):
+			response = sns_client.publish(
+				TopicArn=os.environ["SNSArn"],
+				Message=f"Garage door has been open for {event['queryStringParameters']['open_duration']} minutes",
+				Subject='Door Activity'
+			)
+		else:
+			return {
+				"statusCode": 400,
+				"body": json.dumps({
+					"message": json.dumps({"status": "Bad Request Error", "message": "Malformed client request. Please provide one door_status, and one open_duration (if applicable) in params"}),
+				}),
+			}
+
+		return {
+			"statusCode": 200,
+			"body": json.dumps({
+				"message": "Message sent successfully",
+			}),
+		}
+	except Exception as e:
+		return {
+			"statusCode": 500,
+			"body": json.dumps({
+				"message": json.dumps({"status": "Lambda Error", "message": f"Lambda error message - {str(e)}"}),
+			}),
+		}
